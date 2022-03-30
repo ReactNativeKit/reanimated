9f8ca622ba6c0ff1bd180d3852eaad103e1c82c7
Abhinav Anand <abhi9vanand@gmail.com>
2022-03-30 14:39:47 +0530

    files added

---
+++ App.js
@@ -0,0 +1,73 @@
+import React, { useEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withRepeat,
+  withTiming,
+  interpolate,
+} from "react-native-reanimated";
+
+const Ring = ({ delay }) => {
+  const ring = useSharedValue(0);
+
+  const ringStyle = useAnimatedStyle(() => {
+    return {
+      opacity: 0.8 - ring.value,
+      transform: [
+        {
+          scale: interpolate(ring.value, [0, 1], [0, 4]),
+        },
+      ],
+    };
+  });
+  useEffect(() => {
+    ring.value = withDelay(
+      delay,
+      withRepeat(
+        withTiming(1, {
+          duration: 4000,
+        }),
+        -1,
+        false
+      )
+    );
+  }, []);
+  return <Animated.View style={[styles.ring, ringStyle]} />;
+};
+
+export default function AnimatedRingExample() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        backgroundColor:"#F7E2E2"
+      }}
+    >
+      <Ring delay={0} />
+      <Ring delay={1000} />
+      <Ring delay={2000} />
+      <Ring delay={3000} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: "#F10086",
+  },
+  ring: {
+    position: "absolute",
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    borderColor: "#F10086",
+    borderWidth: 10,
+  },
+});
